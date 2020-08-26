66a9ab1f214acf34441f93430a8dab5fb59c23c9
jocafernanro <jocafernanro@gmail.com>
2020-08-26 20:04:12 +0200

    mini footer size fix

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -1,6 +1,8 @@
 <template>
-  <footer class="mt-24 mb-10 text-center">
-    <span class="text-gray-400 text-xs">josematico.com @jocafernanro</span>
+  <footer class="mt-12 mb-4 md:mt-24 md:mb-10 text-center">
+    <span class="text-gray-500 text-sm sm:text-lg"
+      >josematico.com @jocafernanro</span
+    >
   </footer>
 </template>
 
